56df7ed2c555672eca98a9da606413f719a9b75d
StrangerODua <rooderuler@gmail.com>
2022-05-14 12:45:33 +0300

    complete

--- homework-12/index.js
+++ homework-12/index.js
@@ -2,7 +2,7 @@
 //Вам нужно, написать функцию, которая принимает 1 параметр. При первом вызове,
 // она его запоминает, при втором — суммирует переданный параметр с тем, что передали
 // первый раз и тд. Всё это с замыканиями, например: sum(3) = 3 sum(5) = 8 sum(20) = 28
-function counter() {
+function accumulator() {
     let count;
 
     return (number) => {
@@ -16,11 +16,11 @@ function counter() {
     };
 }
 
-const additionNums = counter();
+const someAcc = accumulator();
 
-console.log(additionNums(1));
-console.log(additionNums(2));
-console.log(additionNums(3));
+console.log(someAcc(1));
+console.log(someAcc(2));
+console.log(someAcc(3));
 
 
 
@@ -28,22 +28,18 @@ console.log(additionNums(3));
 //Это не все. Возьмите счетчик, который, мы писали в классе и добавьте ему
 // возможность задавать начальное значение и шаг счетчика при инициализации и
 // методах для сброса к начальному значению. Помните, что функция - это объект)
-function advancedCounter(initialCount, step) {
+function counter(initialCount, step) {
     this.count = initialCount;
 
-    return function () {
-        return this.count += step;
-    };
+    return () => this.count += step;
 }
 
-const createCounter = advancedCounter(100, 5);
-createCounter.resetCounter = () => {
-    this.count = 0
-}
-const resetCounter = createCounter.resetCounter.bind(advancedCounter)
+const someCounter = counter(100, 5);
+someCounter.reset = () => this.count = 0
+const resetSomeCounter = someCounter.reset.bind(counter)
 
-console.log(createCounter());
-console.log(createCounter());
-resetCounter()
-console.log(createCounter());
-console.log(createCounter());
+console.log(someCounter());
+console.log(someCounter());
+resetSomeCounter()
+console.log(someCounter());
+console.log(someCounter());

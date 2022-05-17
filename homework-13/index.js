3d16d0258440281abbfc93a647802092feff1c5a
StrangerODua <rooderuler@gmail.com>
2022-05-17 16:29:08 +0300

    complete

--- homework-13/index.js
+++ homework-13/index.js
@@ -7,6 +7,13 @@ function findPalindrome(number) {
             let firstPart = number;
             let reversePart = 0n;
 
+            if (number < 10) {
+                return {
+                    result: number,
+                    steps,
+                };
+            }
+
             const cutLastSymbol = (num) => BigInt(num.toString().slice(0, -1));
 
             while (firstPart > reversePart) {
@@ -27,7 +34,6 @@ function findPalindrome(number) {
             const reverseNumber = BigInt(number.toString().split('').reverse().join(''));
             number += reverseNumber;
             ++steps;
-
             return search(number);
         } catch (e) {
             return e.message;
@@ -37,4 +43,10 @@ function findPalindrome(number) {
     return search();
 }
 
-console.log(findPalindrome(196));
+console.log(findPalindrome(9));
+console.log(findPalindrome(12));
+console.log(findPalindrome(59));
+console.log(findPalindrome(96));
+console.log(findPalindrome(79));
+console.log(findPalindrome(167));
+console.log(findPalindrome(89));

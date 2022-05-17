f51dd72a0c1b362973b000e5ffe0d20ece0bb4d6
StrangerODua <rooderuler@gmail.com>
2022-05-17 15:02:23 +0300

    complete

--- homework-13/index.js
+++ homework-13/index.js
@@ -2,31 +2,31 @@ function findPalindrome(number) {
     let steps = 0;
 
     function search() {
-        let firstPart = number;
-        let reversePart = 0;
-
-        while (firstPart > reversePart) {
-            reversePart *= 10;
-            reversePart += firstPart % 10;
-            firstPart = Math.trunc(firstPart / 10);
-        }
-
-        if (firstPart === reversePart || firstPart === Math.trunc(reversePart / 10)) {
-            return {
-                result: number,
-                steps,
-            };
-        }
-
-        const reverseNumber = Number(number.toString().split('').reverse().join(''));
-        number += reverseNumber;
-        ++steps;
-
-        if (number > 2 ** 53 - 1 || reverseNumber > 2 ** 53 - 1) {
-            return `restriction: "Number" type value can't be bigger than 2 ** 53 - 1`;
-        }
-
         try {
+            let firstPart = number;
+            let reversePart = 0;
+
+            while (firstPart > reversePart) {
+                reversePart *= 10;
+                reversePart += firstPart % 10;
+                firstPart = Math.trunc(firstPart / 10);
+            }
+
+            if (firstPart === reversePart || firstPart === Math.trunc(reversePart / 10)) {
+                return {
+                    result: number,
+                    steps,
+                };
+            }
+
+            const reverseNumber = Number(number.toString().split('').reverse().join(''));
+            number += reverseNumber;
+            ++steps;
+
+            if (number > 2 ** 53 - 1 || reverseNumber > 2 ** 53 - 1) {
+                return `restriction: "Number" type value can't be bigger than 2 ** 53 - 1`;
+            }
+
             return search(number);
         } catch (e) {
             return `Error, ${e.message}, call stack overflow on ${steps} iteration`;

741a799a5fa4638b44820163e64d7bbecd81578b
StrangerODua <rooderuler@gmail.com>
2022-05-18 10:07:51 +0300

    complete

--- homework-13/index.js
+++ homework-13/index.js
@@ -3,29 +3,34 @@ function findPalindrome(number) {
     number = BigInt(number);
 
     function search(number) {
-        if (number < 0) {
-            return `Number can't be smaller than 0`;
-        }
+        try {
+            if (number < 0) {
+                return `Number can't be smaller than 0`;
+            }
 
-        if (isPalindrome(number)) {
-            return {
-                result: number,
-                steps,
-            };
-        }
+            if (isPalindrome(number)) {
+                return {
+                    result: number,
+                    steps,
+                };
+            }
 
-        function isPalindrome(number) {
-            let num = number.toString();
-            if (num.length === 1) return true;
-            if (num.length === 2) return num[0] === num[1];
-            if (num[0] === num.slice(-1)) return isPalindrome(num.slice(1, -1));
-            return false;
-        }
+            function isPalindrome(number) {
+                let num = number.toString();
+                if (num.length === 1) return true;
+                if (num.length === 2) return num[0] === num[1];
+                if (num[0] === num.slice(-1)) return isPalindrome(num.slice(1, -1));
+                return false;
+            }
 
-        ++steps;
-        const reverseNumber = BigInt(number.toString().split('').reverse().join(''));
-        const nextNumber = number + reverseNumber;
-        return search(nextNumber);
+            ++steps;
+            const reverseNumber = BigInt(number.toString().split('').reverse().join(''));
+            const nextNumber = number + reverseNumber;
+            return search(nextNumber);
+
+        } catch (e) {
+            return e.message;
+        }
     }
 
     return search(number);
@@ -39,4 +44,4 @@ console.log(findPalindrome(96));
 console.log(findPalindrome(79));
 console.log(findPalindrome(167));
 console.log(findPalindrome(89));
-// console.log(findPalindrome(196));
+console.log(findPalindrome(196));

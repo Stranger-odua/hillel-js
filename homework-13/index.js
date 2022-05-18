4db8455557323e6a4781c4b8beec6931d3c4c694
StrangerODua <rooderuler@gmail.com>
2022-05-18 10:04:26 +0300

    complete

--- homework-13/index.js
+++ homework-13/index.js
@@ -1,59 +1,43 @@
 function findPalindrome(number) {
     let steps = 0;
 
-    if (number < 10) {
-        return {
-            result: number,
-            steps,
-        };
-    }
+    function search(number) {
+        number = BigInt(number)
 
-    if (number < 0) {
-        return `Can't be smaller than 0`;
-    }
-
-    function search() {
-        try {
-            number = BigInt(number);
-            let firstPart = number;
-            let reversePart = 0n;
-
-            const cutLastSymbol = (num) => BigInt(num.toString().slice(0, -1));
-
-            while (firstPart > reversePart) {
-                reversePart *= 10n;
-                reversePart += firstPart % 10n;
-                firstPart = cutLastSymbol(firstPart);
-            }
-
-            const arePartsIsEqual = firstPart === reversePart || firstPart === cutLastSymbol(reversePart);
-
-            if (arePartsIsEqual) {
-                return {
-                    result: number,
-                    steps,
-                };
-            }
-
-            const reverseNumber = BigInt(number.toString().split('').reverse().join(''));
-            number += reverseNumber;
-            ++steps;
-
-            return search(number);
-        } catch (e) {
-            return e.message;
+        if (number < 0) {
+            return `Number can't be smaller than 0`;
         }
+
+        if (isPalindrome(number)) {
+            return {
+                result: number,
+                steps,
+            };
+        }
+
+        function isPalindrome(number) {
+            let num = number.toString()
+            if (num.length === 1) return true;
+            if (num.length === 2) return num[0] === num[1];
+            if (num[0] === num.slice(-1)) return isPalindrome(num.slice(1, -1));
+            return false;
+        }
+
+        ++steps
+        const reverseNumber = BigInt(number.toString().split('').reverse().join(''));
+        const nextNumber = number + reverseNumber
+        return search(nextNumber)
     }
 
-    return search();
+    return search(number)
 }
 
 console.log(findPalindrome(9));
 console.log(findPalindrome(12));
 console.log(findPalindrome(59));
+console.log(findPalindrome(88));
 console.log(findPalindrome(96));
 console.log(findPalindrome(79));
 console.log(findPalindrome(167));
 console.log(findPalindrome(89));
-
 // console.log(findPalindrome(196));

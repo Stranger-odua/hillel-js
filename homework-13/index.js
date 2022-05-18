99b29de911e16c19e0f75ea816a53cc899d43121
StrangerODua <rooderuler@gmail.com>
2022-05-18 10:05:40 +0300

    complete

--- homework-13/index.js
+++ homework-13/index.js
@@ -1,9 +1,8 @@
 function findPalindrome(number) {
     let steps = 0;
+    number = BigInt(number);
 
     function search(number) {
-        number = BigInt(number)
-
         if (number < 0) {
             return `Number can't be smaller than 0`;
         }
@@ -16,20 +15,20 @@ function findPalindrome(number) {
         }
 
         function isPalindrome(number) {
-            let num = number.toString()
+            let num = number.toString();
             if (num.length === 1) return true;
             if (num.length === 2) return num[0] === num[1];
             if (num[0] === num.slice(-1)) return isPalindrome(num.slice(1, -1));
             return false;
         }
 
-        ++steps
+        ++steps;
         const reverseNumber = BigInt(number.toString().split('').reverse().join(''));
-        const nextNumber = number + reverseNumber
-        return search(nextNumber)
+        const nextNumber = number + reverseNumber;
+        return search(nextNumber);
     }
 
-    return search(number)
+    return search(number);
 }
 
 console.log(findPalindrome(9));

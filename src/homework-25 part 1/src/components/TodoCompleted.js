2d4452b12f43d49b41016d74648e15f93fa39fff
strangerODua <rooderuler@gmail.com>
2022-08-20 02:16:50 +0300

    complete

--- src/homework-25 part 1/src/components/TodoCompleted.js
+++ src/homework-25 part 1/src/components/TodoCompleted.js
@@ -1,8 +1,5 @@
 function TodoCompleted({clearCompletedTodos, itemsCount}) {
-
     const style = itemsCount.completed > 0 ? {display: 'block'} : {display: 'none'};
-
-    console.log(itemsCount.completed);
 
     return <button
         className="clear-completed"
